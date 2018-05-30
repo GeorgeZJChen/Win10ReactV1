0312654aa26ea2d2834e11fe9fb94ac1bd311106
GeorgeZJChen <georgechenzj@outlook.com>
2018-05-30 03:02:07 +0100

    update start menu box view

--- app/src/main.js
+++ app/src/main.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {render} from 'react-dom';
 import Login from './js/login/login.js';
 // import Loader from './js/components/loader.js'
-// setTimeout(function functionName() {
-//   render(<Login parentId='win10_login'/>, document.getElementById('win10_login'));
-// }, 500)
+setTimeout(function functionName() {
+  render(<Login parentId='win10_login'/>, document.getElementById('win10_login'));
+}, 500)
 
-import Desktop from './js/desktop/desktop.js';
-render(<div></div>, document.getElementById('win10_login'));
-render(<Desktop/>, document.getElementById('win10_main'))
+// import Desktop from './js/desktop/desktop.js';
+// render(<div></div>, document.getElementById('win10_login'));
+// render(<Desktop/>, document.getElementById('win10_main'))
 
 
 
