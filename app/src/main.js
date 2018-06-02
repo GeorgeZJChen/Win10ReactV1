56b78830e1e7fae29d6ca8699df92ba6880b87e4
GeorgeZJChen <georgechenzj@outlook.com>
2018-06-02 23:30:57 +0100

    enabled selecting desktop items

--- app/src/main.js
+++ app/src/main.js
@@ -16,16 +16,19 @@ setTimeout(function functionName() {
 
 
 window.onload=function () {
-        document.addEventListener('touchstart',function (e) {
-          e.preventDefault()
+        document.addEventListener('touchstart',function (event) {
+            if(event.touches.length>1){
+                event.preventDefault();
+            }
         })
         var lastTouchEnd=0;
-        document.addEventListener('touchend',function (e) {
-          e.preventDefault()
-        })
-        document.addEventListener('touchmove', function(e){
-          e.preventDefault()
-        })
+        document.addEventListener('touchend',function (event) {
+            var now=(new Date()).getTime();
+            if(now-lastTouchEnd<=300){
+                event.preventDefault();
+            }
+            lastTouchEnd=now;
+        },false)
     }
 
 
